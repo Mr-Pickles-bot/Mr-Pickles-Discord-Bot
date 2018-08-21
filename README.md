687d0cb9b0e30b3b89707fe43292fd3154dab0ce
MazariFox~ <mazarifox@gmail.com>
2018-08-21 16:46:58 -0500

    repo move

---
+++ README.md
@@ -0,0 +1,2 @@
+# mr-pickles-bot
+A work-in-progress Discord Bot made in Python by JoshTheGamer and MazariFox.
---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title Mr Pickles Discord Bot
+echo Starting...
+
+:x
+%SYSTEMROOT%\py.exe -3 bot.py
+goto x
