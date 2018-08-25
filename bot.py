383ed04224822c34cb3e53c81721f7baa2a781b1
Joshua Griffin <JoshTheGamer632@outlook.com>
2018-08-25 13:59:42 -0400

    Added discord bot invite to info command

--- bot.py
+++ bot.py
@@ -145,6 +145,7 @@ async def info():
     embed.add_field(name=":busts_in_silhouette: Developers", value="**MZFX18#0069 & JoshTheGamer632#0017**", inline=True)
     embed.add_field(name="<:github:425761614441218048> GitHub", value="https://github.com/Mr-Pickles-bot/Mr.-Pickles-Discord-Bot", inline=True)
     embed.add_field(name="<:kingwumpus:425762228667416625> Need Support?", value="https://discord.gg/jqDH5wZ", inline=True)
+    embed.add_field(name=":link: Invite the bot", value:"https://bot.discord.io/mrpickles", inline=True)
     embed.set_footer(text="That was info about my vaccum cleaner.")
     await bot.say(embed=embed)
 
